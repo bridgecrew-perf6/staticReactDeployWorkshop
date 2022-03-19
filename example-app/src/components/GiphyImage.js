775ef7b1c660355a274f0a0a47003aecc032c209
thaiscmky <thaiscmky@github.com>
2022-03-19 12:21:40 -0400

    Fixes Giphy image not loading

--- example-app/src/components/GiphyImage.js
+++ example-app/src/components/GiphyImage.js
@@ -2,12 +2,10 @@ import React from 'react';
 
 const DisplayAnImage = () => {
   return (
-      <img
-        source={{
-          uri: 'https://media.giphy.com/media/nJ6yoH4nBNCBa/giphy.gif',
-        }}
-        alt="Giphy Motivation"
-      />
+      <>
+      <iframe src="https://giphy.com/embed/nJ6yoH4nBNCBa" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+      <p><a href="https://giphy.com/gifs/ShalitaGrant-point-pointing-nJ6yoH4nBNCBa">via GIPHY</a></p>
+      </>
   );
 }
 

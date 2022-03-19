a12ecf9409c5445a5138276d08999af38bf88f28
thaiscmky <thaiscmky@github.com>
2022-03-19 12:12:50 -0400

    Update text in App component

--- example-app/src/App.js
+++ example-app/src/App.js
@@ -7,16 +7,37 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          There are several places you can learn React from:
         </p>
-        <a
+        <ul>
+          <li>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            ReactJs.Org
+          </a>
+          </li>
+          <li>
+          <a
+            className="App-link"
+            href="https://create-react-app.dev/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Create-React-App.dev
+          </a>
+          </li>
+        </ul>
+        <hr/>
+        The example component you see here was obtained from <a 
           className="App-link"
-          href="https://reactjs.org"
+          href="https://create-react-app.dev/docs/adding-images-fonts-and-files"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          ><em>Adding Images, Fonts, and Files</em></a>
       </header>
     </div>
   );
